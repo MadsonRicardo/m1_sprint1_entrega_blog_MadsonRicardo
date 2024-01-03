c6d831c793b1d1cbbd2b65b79b0f1aea0b87b2c5
Madson de Miranda <madson_miranda@hotmail.com.br>
2024-01-03 17:09:36 -0400

    Código Reduzido

--- script.js
+++ script.js
@@ -1,15 +1,7 @@
 function playRockPaperScissor(player1, player2) {
-    if (player1 === 'Pedra' && player2 === 'Tesoura') {
+    if (player1 === 'Pedra' && player2 === 'Tesoura' || player1 === 'Tesoura' && player2 === 'Papel' || player1 === 'Papel' && player2 === 'Pedra' ) {
         return `Jogador 1 venceu!`
-    } else if (player1 === 'Tesoura' && player2 === 'Papel') {
-        return `Jogador 1 venceu!`
-    } else if (player1 === 'Papel' && player2 === "Pedra") {
-        return `Jogador 1 venceu!`
-    } else if (player2 === 'Pedra' && player1 === 'Tesoura') {
+    }  else if (player2 === 'Pedra' && player1 === 'Tesoura' || player2 === 'Papel' && player1 === 'Pedra' || player2 === 'Tesoura' && player1 === 'Papel') {
         return `Jogador 2 venceu!`
-    } else if (player2 === 'Papel' && player1 === 'Pedra') {
-        return `Jogador 2 venceu!`
-    } else if (player2 === 'Tesoura' && player1 === 'Papel') {
-        return `Jogador 2 venceu!`
-    } return `Empate!`
+    }  return `Empate!`
 }
